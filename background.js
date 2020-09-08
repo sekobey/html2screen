d33f448656cd474e968a98a75eaeb6ce24fe27f3
Serkan Yildirim <serkanyildirim@gmail.com>
2020-09-08 23:56:00 +0300

    Added audio recording support

--- background.js
+++ background.js
@@ -22,11 +22,17 @@ chrome.runtime.onConnect.addListener(port => {
         chrome.desktopCapture.chooseDesktopMedia(['tab', 'audio'], streamId => {
           // Get the stream
           navigator.webkitGetUserMedia({
-            audio: false,
+            audio: {
+              mandatory: {
+                      chromeMediaSource: 'desktop',
+                      chromeMediaSourceId: streamId
+                  }
+            },
             video: {
               mandatory: {
                 chromeMediaSource: 'desktop',
-                chromeMediaSourceId: streamId
+                chromeMediaSourceId: streamId,
+                maxFrameRate:30
               }
             }
           }, stream => {
